6c746686b1179405a4ffcc4b8d2a1588e6b784e9
AbusufiyanJ <abujafarullah1996@gmail.com>
2019-01-11 12:45:21 +0530

    The Query selector

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-const wrap = document.querySelectorAll('wrapper');
-console.log(wrap);  
+const wmf = document.querySelectorAll('#book-list li:nth-child(2) span.name')
+// console.log(wrap)
+
+var books = document.querySelectorAll('#book-list li .name')
+// console.log(books)
+
+books = document.querySelector('#book-list li .name ')
+// console.log(books)
+
+Array.from(books).forEach(function (book) {
+  console.log(book)
+})
