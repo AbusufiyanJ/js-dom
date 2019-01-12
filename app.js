04f8a8da0e707244bcb8aadb6098aaa535055db9
AbusufiyanJ <abujafarullah1996@gmail.com>
2019-01-12 14:29:10 +0530

    perfect js code

--- app.js
+++ app.js
@@ -1,19 +1,1 @@
-const wmf = document.querySelectorAll('#book-list li:nth-child(2) span.name')
-// console.log(wrap)
-
 var books = document.querySelectorAll('#book-list li .name')
-// console.log(books)
-
-books = document.querySelector('#book-list li .name ')
-// console.log(books)
-
- // Array.from(books).forEach(function (book) {
-  console.log(book)
- })
-
- 
-
-
-var books = document.querySelectorAll('#book-list li .name')
-
-
