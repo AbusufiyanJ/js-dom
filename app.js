dd355027fe2a454d6f67ef8d99d224af4e6a5f24
AbusufiyanJ <abujafarullah1996@gmail.com>
2019-01-12 22:00:00 +0530

    added nodes in app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+const banner = document.querySelector('#page-banner')
+
+console.log('#page-banner node type is:', banner.nodeType)
+
+console.log('#page-banner node name is:', banner.nodeName)
+
+console.log('#page-banner child node:', banner.hasChildNodes())
+
+const clonedBanner = banner.cloneNode(true)
+console.log(clonedBanner)
