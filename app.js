36c13bd683e3247622cef0d668df4f4c7118a10e
AbusufiyanJ <abujafarullah1996@gmail.com>
2019-01-13 18:08:33 +0530

    finished parent and child nodes

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-const bookList = document.querySelector('#book-list')
+const bookList = document.querySelector('#book-list ')
 
-console.log('the parent node is:', bookList.parentNode)
-console.log('the parent Element is:', bookList.parentElement.parentElement)
+console.log('the next sibling node is:', bookList.nextSibling)
+console.log('the next sibling element node is:', bookList.nextElementSibling)
 
-console.log(bookList.childNodes)
+console.log('book-list previous sibiling is:', bookList.previousSibling)
+console.log('the previous sibiling element node is:', bookList.previousElementSibling)
+
+console.log('the previous sibiling2 element node is:', bookList.previousElementSibling.querySelector('p').innerHTML += '<br> Too cool for everyone else!')
