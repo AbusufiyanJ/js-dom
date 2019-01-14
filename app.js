68ddf1acdade94c5091f0f55b78d919194667776
AbusufiyanJ <abujafarullah1996@gmail.com>
2019-01-14 12:47:12 +0530

    final of tuto 9

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ var btns = document.querySelectorAll('#book-list .delete')
 
 Array.from(btns).forEach(function (btn) {
   btn.addEventListener('click', function (e) {
-    const li = e.target.parentElement;
+    const li = e.target.parentElement
 
     li.parentNode.removeChild(li)
-  });
-});
+  })
+})
+
+const link = document.querySelector('#page-banner a')
+
+link.addEventListener('click', function (e) {
+  e.preventDefault();
+  console.log('navigation to ',e.target.textContent,'was prevented');
+})
