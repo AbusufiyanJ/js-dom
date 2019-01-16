e5eaa9e9a7de2e20973491e7b60d78afa628899b
AbusufiyanJ <abujafarullah1996@gmail.com>
2019-01-16 23:38:41 +0530

    changed js code

--- app.js
+++ app.js
@@ -14,3 +14,14 @@ link.addEventListener('click', function (e) {
   e.preventDefault();
   console.log('navigation to ',e.target.textContent,'was prevented');
 })
+
+const banner = document.querySelector('#page-banner')
+
+console.log('#page-banner node type is:', banner.nodeType)
+
+console.log('#page-banner node name is:', banner.nodeName)
+
+console.log('#page-banner child node:', banner.hasChildNodes())
+
+const clonedBanner = banner.cloneNode(true)
+console.log(clonedBanner)
