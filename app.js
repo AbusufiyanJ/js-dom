c5fb2dfd6c38ab1c3288c7e18764c08a88af34bb
AbusufiyanJ <abujafarullah1996@gmail.com>
2019-01-16 23:40:23 +0530

    banner added in js code

--- app.js
+++ app.js
@@ -8,5 +8,20 @@ Array.from(btns).forEach(function (btn) {
   })
 })
 
+const link = document.querySelector('#page-banner a')
 
+link.addEventListener('click', function (e) {
+  e.preventDefault();
+  console.log('navigation to ',e.target.textContent,'was prevented');
+})
 
+const banner = document.querySelector('#page-banner')
+
+console.log('#page-banner node type is:', banner.nodeType)
+
+console.log('#page-banner node name is:', banner.nodeName)
+
+console.log('#page-banner child node:', banner.hasChildNodes())
+
+const clonedBanner = banner.cloneNode(true)
+console.log(clonedBanner)
