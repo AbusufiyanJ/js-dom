7c3b59a48cf96a8ebf995484bafd66cefe6f1f50
AbusufiyanJ <abujafarullah1996@gmail.com>
2019-01-16 23:39:51 +0530

    removed link in js code

--- app.js
+++ app.js
@@ -8,11 +8,5 @@ Array.from(btns).forEach(function (btn) {
   })
 })
 
-const link = document.querySelector('#page-banner a')
-
-link.addEventListener('click', function (e) {
-  e.preventDefault();
-  console.log('navigation to ',e.target.textContent,'was prevented');
-})
 
 
